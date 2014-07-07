1f78265748398ee10494b3619b6fb58f0c47f809
anandcv <anand.mallaya@effte.co>
2014-07-07 12:32:39 +0530

    updated from editor

--- Sma.java
+++ Sma.java
@@ -36,8 +36,8 @@ public class Sma extends Strategy {
             context.setPortfolioValue(BigDecimal.valueOf(150000));
             context.setDataType(Event.Type.BAR);
 
-            context.setStartDate("01-01-2012");
-            context.setEndDate("05-01-2012");
+            context.setStartDate("01-01-2013");
+            context.setEndDate("05-01-2013");
 
         }
 
